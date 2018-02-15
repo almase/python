8bfc5453e4e212377c3253d47508c659b3fdfc32
Alberto <alberto.puche@gmail.com>
2018-02-15 16:30:14 +0100

    kkk

---
+++ ejer7.py
@@ -0,0 +1,13 @@
+import mis_funciones
+
+
+#mis_funciones.imprimir()
+
+#print mis_funciones.par_impar(30)
+
+#print mis_funciones.par_impar1(30)
+
+#mis_funciones.alta_cliente(1,'pepe','orororo','oooo','89898989')
+
+print mis_funciones.iniciales('Alberto','Rodriguez','Martinez')
+print mis_funciones.iniciales('Alberto','Rodriguez','Martinez','Leonidas')
